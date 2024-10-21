2f1711936de62ae000e5dcfb0078ca5a97e769d8
Fabian Seidler <fabian.seidler@eaps.ethz.ch>
2024-10-21 15:25:04 +0200

    Inital commit (messy)

---
+++ phaethon/celestial_objects.py
@@ -0,0 +1,463 @@
+"""
+Module for managing the parameters physical and orbital paramters of the planet and its star. 
+"""
+
+import logging
+import os
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import Callable, Union
+
+import astropy.constants as const
+import astropy.units as unit
+import numpy as np
+from astropy.units.core import Unit as AstropyUnit
+from astropy.units.quantity import Quantity as AstropyQuantity
+from helios.star_tool.functions import main_loop as startool_mainloop
+from scipy.interpolate import interp1d
+
+logger = logging.getLogger(__name__)
+
+# ================================================================================================
+#   STAR
+# ================================================================================================
+
+
+class Star:
+    """
+    Class to store stellar parameters, as well as to load and process spectra
+    """
+
+    name: str
+    mass: float
+    radius: float
+    t_eff: float
+    distance: float
+    metallicity: float
+    logg: float
+    file_or_blackbody: bool
+    source_file: str
+    spectral_emittance_fitfunc: Callable
+
+    def __init__(
+        self,
+        name: str,
+        mass: float,
+        radius: float,
+        t_eff: float,
+        distance: float,
+        metallicity: float,
+    ) -> None:
+        self.name = name
+        self.mass = mass
+        self.radius = radius
+        self.t_eff = t_eff
+        self.distance = distance
+        self.metallicity = metallicity
+
+        # derived params
+        grav: float = const.G * self.mass.to("kg") / (self.radius.to("m") ** 2)
+        self.logg = np.log10(grav.to("cm / s**2").value)
+
+        # default init
+        self.file_or_blackbody: str = "blackbody"
+        self.source_file: str = None
+        self.spectral_emittance_fitfunc: Callable = None
+
+        # stellar spectrum
+        self._orig_wavl: np.ndarray = np.zeros(0)
+        self._orig_spectral_emittance: np.ndarray = np.zeros(0)
+
+        self.wavl: np.ndarray = np.zeros(0)
+        self.spectral_emittance: np.ndarray = np.zeros(0)
+
+        self.path_in_h5 = "./"
+
+    def get_info(self) -> dict:
+        """Returns a dictionary containing information about the star."""
+        _dict = self.__dict__.copy()
+        info_dict = {}
+        for key, value in _dict.items():
+            if isinstance(value, (AstropyUnit, AstropyQuantity)):
+                info_dict[key] = value.value
+            elif isinstance(value, (np.ndarray, Callable)):
+                continue
+            else:
+                info_dict[key] = value
+
+        return info_dict
+
+    def get_spectrum_from_file(
+        self,
+        source_file: os.PathLike,
+        outdir: os.PathLike,
+        opac_file_for_lambdagrid: os.PathLike,
+        plot_and_tweak: bool = False,
+        skiprows: int = 0,
+        w_conversion_factor: float = 1,
+        flux_conversion_factor: float = 1,
+    ):
+        """
+        Reads and processes a spectrum from a file such that HELIOS can read it.
+
+        Parameters
+        ----------
+            source_file : os.PathLike
+                File to read-in.
+            outdir: os.PathLike
+                Where to store the final stellar spectrum file.
+            opac_file_for_lambdagrid: os.PathLike
+                An opacity file for a future HELIOS run as reference to evaluate the star's
+                spectrum on the right wavelength grid.
+            plot_and_tweak: bool
+                Make star_tool plot the final spectrum,
+                allowing to manually tweak it (maybe necessary for phoenix,
+                especially for mid-to-far infrared wavelengths)
+            skiprows : int
+                Number of header rows to skip.
+            w_conversion_factor: float
+                Wavelength conversion factor.
+            flux_conversion_factor : float
+                Flux units conversion factor.
+        """
+        if not outdir.endswith("/"):
+            outdir += "/"
+
+        if not os.path.isdir(outdir):
+            os.makedirs(outdir, exist_ok=True)
+
+        self.source_file = source_file
+
+        startool_params = {
+            "data_format": "ascii",
+            "source_file": self.source_file,
+            "name": self.name,
+            "w_conversion_factor": w_conversion_factor,
+            "flux_conversion_factor": flux_conversion_factor,
+            "temp": self.t_eff.value,
+            "distance_from_Earth": self.distance / unit.parsec,
+            "R_star": self.radius / const.R_sun,
+        }
+        startool_output_file = outdir + self.name + ".h5"
+        _orig_wavl, _orig_spectral_emittance, wavl, converted_flux = startool_mainloop(
+            startool_params,
+            convert_to="r50_kdistr",
+            opac_file_for_lambdagrid=opac_file_for_lambdagrid,
+            output_file=startool_output_file,
+            plot_and_tweak="no" if not plot_and_tweak else "yes",
+            save_in_hdf5="yes",
+            skiprows=skiprows,
+        )
+
+        self.file_or_blackbody = "file"
+        self.path_to_h5 = startool_output_file
+        self.path_in_h5 = "r50_kdistr/ascii/" + self.name
+
+        self._orig_wavl = np.array(_orig_wavl) * 1e4  # from Angstroem to micron
+        self.wavl = np.array(wavl) * 1e4
+        self._orig_spectral_emittance = np.array(_orig_spectral_emittance) * 1e-13
+        self.spectral_emittance = np.array(converted_flux) * 1e-13
+        self.spectral_emittance_fitfunc = interp1d(
+            self._orig_wavl,
+            self._orig_spectral_emittance,
+            bounds_error=False,
+            fill_value=0.0,
+        )
+
+    def get_phoenix_spectrum(
+        self,
+        outdir: str,
+        opac_file_for_lambdagrid: str,
+        plot_and_tweak: bool = False,
+    ) -> None:
+        """
+        Obtain spectrum from Phoenix.
+
+        Parameters
+        ----------
+            outdir : str
+                Where to place the output file
+            opac_file_for_lambdagrid : str
+                Reference for wavelength - must be same as planet
+            plot_and_tweak: bool
+                Make star_tool plot the final spectrum,
+                allowing to manually tweak it (maybe necessary for phoenix,
+                especially for mid-to-far infrared wavelengths)
+        """
+        if not outdir.endswith("/"):
+            outdir += "/"
+
+        if not os.path.isdir(outdir):
+            os.makedirs(outdir, exist_ok=True)
+
+        startool_params = {
+            "data_format": "phoenix",
+            "name": self.name,
+            "temp": self.t_eff.value,
+            "log_g": self.logg,
+            "m": self.metallicity.value,
+        }
+
+        startool_output_file = outdir + self.name + ".h5"
+        _orig_wavl, _orig_spectral_emittance, wavl, converted_flux = startool_mainloop(
+            startool_params,
+            convert_to="r50_kdistr",
+            opac_file_for_lambdagrid=opac_file_for_lambdagrid,
+            output_file=startool_output_file,
+            plot_and_tweak="no" if not plot_and_tweak else "yes",
+            save_in_hdf5="yes",
+        )
+
+        self.file_or_blackbody = "file"
+        self.path_to_h5 = startool_output_file
+        self.path_in_h5 = "r50_kdistr/ascii/" + self.name
+
+        self._orig_wavl = np.array(_orig_wavl) * 1e4  # from Angstroem to micron
+        self.wavl = np.array(wavl) * 1e4
+        self._orig_spectral_emittance = np.array(_orig_spectral_emittance) * 1e-13
+        self.spectral_emittance = np.array(converted_flux) * 1e-13
+        self.spectral_emittance_fitfunc = interp1d(
+            self._orig_wavl,
+            self._orig_spectral_emittance,
+            bounds_error=False,
+            fill_value=0.0,
+        )
+
+    def get_muscles_spectrum(
+        self,
+        outdir: str,
+        opac_file_for_lambdagrid: str,
+        plot_and_tweak: bool = False,
+    ) -> None:
+        """
+        Obtain spectrum from MUSCLES.
+
+        Parameters
+        ----------
+            outdir : str
+                Where to place the output file
+            opac_file_for_lambdagrid : str
+                Reference for wavelength - must be same as planet
+            plot_and_tweak: bool
+                Make star_tool plot the final spectrum,
+                allowing to manually tweak it (maybe necessary for muscles,
+                especially for mid-to-far infrared wavelengths)
+        """
+        if not outdir.endswith("/"):
+            outdir += "/"
+
+        if not os.path.isdir(outdir):
+            os.makedirs(outdir, exist_ok=True)
+
+        startool_params = {
+            "data_format": "phoenix",
+            "name": self.name,
+            "temp": self.t_eff.value,
+            "log_g": self.logg,
+            "m": self.metallicity.value,
+        }
+
+        startool_output_file = outdir + self.name + ".h5"
+        _orig_wavl, _orig_spectral_emittance, wavl, converted_flux = startool_mainloop(
+            startool_params,
+            convert_to="r50_kdistr",
+            opac_file_for_lambdagrid=opac_file_for_lambdagrid,
+            output_file=startool_output_file,
+            plot_and_tweak="no" if not plot_and_tweak else "yes",
+            save_in_hdf5="yes",
+        )
+
+        self.file_or_blackbody = "file"
+        self.path_to_h5 = startool_output_file
+        self.path_in_h5 = "r50_kdistr/ascii/" + self.name
+
+        self._orig_wavl = np.array(_orig_wavl) * 1e4  # from Angstroem to micron
+        self.wavl = np.array(wavl) * 1e4
+        self._orig_spectral_emittance = np.array(_orig_spectral_emittance) * 1e-13
+        self.spectral_emittance = np.array(converted_flux) * 1e-13
+        self.spectral_emittance_fitfunc = interp1d(
+            self._orig_wavl,
+            self._orig_spectral_emittance,
+            bounds_error=False,
+            fill_value=0.0,
+        )
+
+
+# ================================================================================================
+#   ORBIT
+# ================================================================================================
+
+
+class Orbit(ABC):
+    """
+    Relates orbital period <-> semi-major axis
+    """
+
+    @abstractmethod
+    def get_period(self, star_mass: AstropyUnit) -> AstropyUnit: ...
+
+    @abstractmethod
+    def get_semimajor_axis(self, star_mass: AstropyUnit) -> AstropyUnit: ...
+
+
+class CircularOrbitFromPeriod(Orbit):
+    """Properties of a circular orbit based on its period."""
+
+    period: AstropyUnit
+
+    def __init__(self, period: AstropyUnit):
+        self.period = period.to("day")
+
+    def get_period(self, star_mass: AstropyUnit) -> AstropyUnit:
+        return self.period
+
+    def get_semimajor_axis(self, star_mass: AstropyUnit) -> AstropyUnit:
+        return np.cbrt(
+            const.G * star_mass.to("kg") * self.period.to("s") ** 2 / (4.0 * np.pi)
+        ).to("AU")
+
+
+class CircularOrbitFromSemiMajorAxis(Orbit):
+    """Properties of a circular orbit based on its semi-major axis."""
+
+    semi_major_axis: AstropyUnit
+
+    def __init__(self, semi_major_axis: AstropyUnit):
+        self.semi_major_axis = semi_major_axis.to("AU")
+
+    def get_period(self, star_mass: AstropyUnit) -> AstropyUnit:
+        """Returns the period of a circular orbit of given semi-major axis and stellar mass.
+
+        Parameters
+        ----------
+            star_mass: astropy.core.units.Quantity
+                Stellar mass.
+
+        Returns
+        -------
+            period : astropy.core.units.Quantity
+                Orbital period of the planet.
+        """
+        return (
+            2
+            * np.pi
+            * np.sqrt(
+                self.semi_major_axis.to("m") ** 3 / (const.G * star_mass.to("kg"))
+            ).to("day")
+        )
+
+    def get_semimajor_axis(self, star_mass: AstropyUnit) -> AstropyUnit:
+        return self.semi_major_axis
+
+
+# ================================================================================================
+#   PLANET
+# ================================================================================================
+
+
+@dataclass
+class Planet:
+    name: str
+    mass: AstropyUnit
+    radius: AstropyUnit
+    bond_albedo: float
+    dilution_factor: float
+    internal_temperature: Union[None, AstropyUnit]
+    grav: Union[None, AstropyUnit] = None
+    temperature: Union[None, AstropyUnit] = None
+
+    def __post_init__(self):
+        self.grav = const.G * self.mass.to("kg") / (self.radius.to("m") ** 2)
+
+    def get_info(self) -> dict:
+        info_dict = self.__dict__.copy()
+        for key, value in info_dict.items():
+            if isinstance(value, (AstropyUnit, AstropyQuantity)):
+                info_dict[key] = value.value
+
+        return info_dict
+
+
+class PlanetarySystem:
+    star: Star
+    planet: Planet
+    orbit: Orbit
+
+    def __init__(
+        self,
+        star: Star,
+        planet: Planet,
+        orbit: Orbit,
+    ):
+        self.star = star
+        self.planet = planet
+        self.orbit = orbit
+
+        self.planet.temperature = self.calc_pl_temp()
+
+    def calc_pl_temp(self) -> AstropyUnit:
+        """
+        Calculates the temperature of the planet.
+
+        Parameters
+        ----------
+            bond_albedo: float
+                Bond Albedo.
+            dilution_factor : float
+                Dilution factor, dimensionless.
+        Returns
+        -------
+            temperature : AstropyUnit
+                Temperature of the planet.
+        """
+        semi_major_axis: AstropyUnit = self.orbit.get_semimajor_axis(
+            star_mass=self.star.mass
+        )
+        return (
+            (1.0 - self.planet.bond_albedo)
+            * self.planet.dilution_factor
+            * (self.star.radius.to("m") / semi_major_axis.to("m")) ** 2
+            * self.star.t_eff.to("K") ** 4
+        ) ** (0.25)
+
+    def get_period(self) -> AstropyUnit:
+        return self.orbit.get_period(star_mass=self.star.mass)
+
+    def get_semimajor_axis(self) -> AstropyUnit:
+        return self.orbit.get_semimajor_axis(star_mass=self.star.mass)
+
+    def set_semimajor_axis_from_pl_temp(self, t_planet: AstropyUnit) -> None:
+        """
+        Calculates the semimajor axis of the planet given its temperature.
+        Updates the internal value (i.e., in self.orbit).
+
+        Parameters
+        ----------
+            t_planet : astropy.units.core.Unit
+                Temperature of the planet
+        Returns
+        -------
+            temperature : AstropyUnit
+                Temperature of the planet.
+        """
+
+        new_semimajor_axis: AstropyUnit = (
+            np.sqrt(self.planet.dilution_factor * (1.0 - self.planet.bond_albedo))
+            * self.star.radius.to("m")
+            * (self.star.t_eff.to("K") / t_planet.to("K")) ** 2
+        ).to("AU")
+        self.orbit = CircularOrbitFromSemiMajorAxis(semi_major_axis=new_semimajor_axis)
+        self.planet.temperature = t_planet
+
+    def get_info(self) -> dict:
+        """Returns info dict"""
+        return {
+            "star": self.star.get_info(),
+            "planet": self.planet.get_info(),
+            "orbit": {
+                "period": self.orbit.get_period(star_mass=self.star.mass).value,
+                "semi_major_axis": self.orbit.get_semimajor_axis(
+                    star_mass=self.star.mass
+                ).value,
+            },
+        }
---
+++ phaethon/gas_mixture.py
@@ -0,0 +1,279 @@
+"""
+A tool to perform all calculations associated to an ideal gas (e.g. converting pressures to mixing
+ratios, determining elemental fractions, producing a FastChem input file).
+
+Copyright 2024 Fabian L. Seidler
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import logging
+from typing import Union
+
+import numpy as np
+import pandas as pd
+import scipy.constants as sc
+from molmass import Formula
+
+logger = logging.getLogger(__name__)
+
+
+def get_stoichiometry(formula: str) -> dict:
+    """
+    Splits any chemical formula into its stoichiometry.
+    """
+    stoichiometry: dict = {}
+    i: int = 0
+    while i < len(formula):
+        if formula[i].isupper():
+            element = formula[i]
+            i += 1
+            count = ""
+            while i < len(formula) and formula[i].islower():
+                element += formula[i]
+                i += 1
+            while i < len(formula) and formula[i].isdigit():
+                count += formula[i]
+                i += 1
+            if count == "":
+                count = "1"
+            stoichiometry[element] = int(count)
+        else:
+            i += 1
+    return stoichiometry
+
+
+def get_compound_mass(formula: str) -> float:
+    """
+    Returns the molar mass of a chemical compound, e.g. SiO2
+
+    Returns
+    -------
+        mass : float
+            mass per molecule, in atomic mass units
+    """
+    stoich: dict = get_stoichiometry(formula)
+    mass: float = 0.0
+    for elem, stoich_coef in stoich.items():
+        mass += stoich_coef * Formula(elem).mass
+
+    return mass
+
+
+class IdealGasMixture:
+    """
+    A class to perform all calculations associated to an ideal gas.
+    """
+
+    # pylint: disable=too-many-instance-attributes
+
+    _p_bar: pd.Series
+    log_p: pd.Series
+    p_total: pd.Series
+    molfrac: pd.Series
+    elem_molfrac: pd.Series
+    mmw: float
+    gas_stoich: pd.DataFrame
+    mol_masses: pd.Series
+    mixing_ratios: pd.Series
+
+    def __init__(self, p_bar: Union[dict, pd.Series]):
+
+        self._p_bar: pd.Series = pd.Series()
+        self.log_p: pd.Series = pd.Series()
+        self.p_total: float = np.nan
+        self.molfrac: pd.Series = pd.Series()
+        self.elem_molfrac: pd.Series = pd.Series()
+        self.mmw: float = np.nan
+        self.gas_stoich: pd.DataFrame = pd.DataFrame()
+        self.mol_masses: pd.Series = pd.Series()
+        self.mixing_ratios: pd.Series = pd.Series()
+
+        self.p_bar: pd.Series = pd.Series(p_bar)
+
+    @property
+    def p_bar(self) -> pd.Series:
+        """Return total pressure of gas mixture"""
+        return self._p_bar
+
+    @p_bar.setter
+    def p_bar(self, p_dict: dict) -> None:
+        """Build gas mixture from partial pressure dict"""
+        self._p_bar = pd.Series(p_dict)
+        self.log_p = np.log10(self._p_bar)
+        self.p_total = self._p_bar.sum()
+        self.gas_stoich = self._calc_stoich(self._p_bar)
+        self.mol_masses = self._calc_molmasses(self._p_bar)
+        self.elem_molfrac = self._calc_elem_molfrac(self.gas_stoich, self._p_bar)
+        self.molfrac = self.p_bar / np.sum(self.p_bar)
+        self.mmw = np.dot(self.mol_masses, self.molfrac)
+        self.mixing_ratios = self._calc_volume_mixing_ratios(self.molfrac)
+
+    def __add__(self, other):
+        """
+        Add two GasMixtures together.
+        """
+        assert isinstance(other, IdealGasMixture)
+        return IdealGasMixture(self.p_bar.add(other.p_bar, fill_value=0.0))
+
+    @staticmethod
+    def _calc_stoich(p_bar: pd.Series) -> pd.DataFrame:
+        """
+        Calculate the stoichiometric matrix, containing all gas species.
+
+        Parameters
+        ----------
+            p_bar : pd.Series
+                Pressures of gas species, in bar.
+        Returns
+        -------
+            gas_stoich : pd.DataFrame
+                Stoichiometric matrix.
+        """
+        gas_stoich = pd.DataFrame()
+
+        for i in range(len(p_bar)):
+            species_formula = p_bar.index[i]
+            species_stoich = pd.Series(get_stoichiometry(species_formula))
+            gas_stoich = pd.concat([gas_stoich, species_stoich], axis=1)
+
+        gas_stoich = gas_stoich.T
+        gas_stoich.index = p_bar.index
+        gas_stoich = gas_stoich.replace(np.nan, 0.0)
+
+        return gas_stoich
+
+    @staticmethod
+    def _calc_molmasses(p_bar: pd.Series) -> pd.Series:
+        """
+        Calculate the molmasses of gas species.
+
+        Parameters
+        ----------
+            p_bar : pd.Series
+                Pressures of gas species, in bar.
+        Returns
+        -------
+            mol_masses : pd.Series
+                Series with molar masses of gas species.
+        """
+        mol_masses = {}
+
+        for i in range(len(p_bar)):
+            species_formula = p_bar.index[i]
+            species_molmass = get_compound_mass(species_formula)
+            mol_masses = mol_masses | {species_formula: species_molmass}
+
+        return pd.Series(mol_masses)
+
+    @staticmethod
+    def _calc_elem_molfrac(gas_stoich: pd.DataFrame, p_bar: pd.Series) -> pd.Series:
+        """
+        Calculate the molar fractions of elements.
+
+        Parameters
+        ----------
+            gas_stoich : pd.DataFrame
+                Stoichiometric matrix.
+            p_bar : pd.Series
+                Pressures of gas species, in bar.
+        Returns
+        -------
+            elem_molfrac : pd.Series
+                Fractions of elements in the gas mixture.
+        """
+        n_gaselem_dimless = np.dot(gas_stoich.T, p_bar)
+
+        elem_molfrac = pd.Series(
+            data=n_gaselem_dimless / np.sum(n_gaselem_dimless),
+            index=gas_stoich.columns,
+        )
+
+        return elem_molfrac
+
+    @staticmethod
+    def _calc_volume_mixing_ratios(molfracs: pd.Series) -> pd.Series:
+        """
+        Calculate the volume mixing ratios of gas species.
+
+        Parameters
+        ----------
+            molfracs : pd.Series
+                Molar fractions of gas species in mixture.
+        Returns
+        -------
+            mixing_ratios : pd.Series
+                Volume mixing ratios of gas species.
+        """
+        mixing_ratios = molfracs.copy()
+        for i in range(len(mixing_ratios)):
+            mixing_ratios.iloc[i] /= 1.0 - mixing_ratios.iloc[i]
+
+        return mixing_ratios
+
+    def to_fastchem(self, outfile: str, reference_element: str) -> None:
+        """
+        Write a fastchem input file.
+
+        Parameters
+        ----------
+            outfile : str
+                Name of the output file.
+            reference_element : str
+                reference element for normalisation
+        """
+
+        assert reference_element in self.elem_molfrac.index
+
+        # molar ratio of elements to reference element
+        xi = self.elem_molfrac.copy()
+        for elem in xi.index:
+            if elem == reference_element:
+                xi[elem] = 1.0
+            else:
+                xi[elem] = (
+                    self.elem_molfrac[elem] / self.elem_molfrac[reference_element]
+                )
+
+        # normalize to 10¹² reference element atoms
+        log_n = np.log10(xi) + 12.0
+
+        # write file
+        with open(outfile, "w", encoding="utf-8") as f:
+            f.write("# This is the header\n")
+            f.write("e-    0.0\n")
+            for elem in self.elem_molfrac.index:
+                if elem in log_n:
+                    value = log_n[elem]
+                else:
+                    value = -np.inf
+                if np.isfinite(value):
+                    f.write(elem + "    " + str(value) + "\n")
+                else:
+                    f.write(elem + "    " + str(0.0) + "\n")
+
+    def col_dens(self, grav: float) -> pd.Series:
+        """
+        Calculates the column densities of the gas species
+
+        Parameters
+        ----------
+            grav : float
+                Gravitational acceleration, in m/s^2.
+        Returns
+        -------
+            col_dens : pd.Series
+                Column density of gas species.
+        """
+        return self.p_bar / (self.mol_masses * sc.Avogadro * sc.u * grav)

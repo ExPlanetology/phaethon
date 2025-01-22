fbafe38be59037d8f75fe2b774108c2e742502a7
Fabian <fabian.seidler@erdw.ethz.ch>
2025-01-22 14:14:33 +0100

    MWE added/updated

--- phaethon_demo.py
+++ phaethon_demo.py
@@ -6,6 +6,9 @@ Hence, this code will not work.
 If you desire to run the script, please obtain a copy of the MAGMA code and modify it according
 to Seidler et al. 2024.
 """
+import pandas as pd
+from typing import Callable, Dict
+from scipy.interpolate import interp1d
 import astropy.units as unit
 
 from phaethon.celestial_objects import (
@@ -18,121 +21,112 @@ from phaethon.outgassing import VapourEngine
 from phaethon.gas_mixture import IdealGasMixture
 from phaethon.pipeline import PhaethonPipeline
 
-class Melt:
+OPACITY_PATH: str = "/path/to/opacities/"
+
+class VapourEngineExample(VapourEngine):
+    """ A simple example to an outgassing routine. Only valid for a fixed oxygen fugacity, but
+    allows for variable temperature (as it must, because the oxidation state of the melt is usually
+    held constant during a simualtion while the temperature of the atmosphere-melt interface
+    changes).
     """
-    Dummy class to manage access to MAGMA.
-    """
 
-    def __init__(self, buffer: str = "IW") -> None:
-        self.buffer = buffer
+    _df: pd.DataFrame
+    """ Frame holding the log of partial pressures of species as function of temperature. """
 
-    def set_chemistry(self, wt: dict = None, mol: dict = None) -> None:
-        """
-        Dummy function to set chemistry in terms of melt oxides.
-        """
-        raise NotImplementedError()
+    _logp_fits: Dict[str, Callable]
+    """ Dicitonary holding `scipy.interpolate.interp1d` instances that relate temperature <-> 
+    outgassing pressure. """
 
-    def vaporise(self, temperature: float, dlogfO2: float) -> IdealGasMixture:
-        """
-        Dummy function to set chemistry in terms of melt oxides.
-        """
-        raise NotImplementedError()
-
-class ModifiedMAGMA(VapourEngine):
-    """ Wrapper to the MAGMA code """
-
-    melt: Melt
-    dlogfO2: float
     vapour: IdealGasMixture
+    """ Object holding the properties of the vapour. """
 
-    def __init__(
-        self,
-        buffer: str,
-        dlogfO2: float,
-        melt_wt_comp: dict = None,
-        melt_mol_comp: dict = None,
-    ):
-        self.melt = Melt(buffer=buffer)
-        self.dlogfO2 = dlogfO2
-
-        if melt_mol_comp is not None and melt_wt_comp is None:
-            self.melt.set_chemistry(mol=melt_mol_comp)
-        elif melt_mol_comp is None and melt_wt_comp is not None:
-            self.melt.set_chemistry(wt=melt_wt_comp)
-        elif melt_mol_comp is not None and melt_wt_comp is not None:
-            raise ValueError(
-                "Provide either 'melt_mol_comp' or 'melt_wt_comp', not both."
-            )
-        else:
-            pass
+    def __init__(self) -> None:
+        self._df = pd.read_csv("input/logP_TERRA_dIW-4.csv", index_col=0)
+        
+        self._logp_fits = {}
+        temp_arr = self._df.columns.to_numpy(dtype=float)
+        for species in self._df.index:
+            self._logp_fits[species] = interp1d(temp_arr, self._df.loc[species].to_numpy())
 
     def get_info(self) -> dict:
-        raise NotImplementedError(
-            "Absent license to MAGMA, implementation must not be shared."
-        )
+        """ Returns information on state of the outgassing routine. """
+        return {
+            "dIW": -4.0,
+            "composition_name": "TERRA"
+        }
 
     def set_extra_params(self, params: dict) -> None:
-        raise NotImplementedError(
-            "Absent license to MAGMA, implementation must not be shared."
-        )
+        """ No extra parameters to set """
+        pass
 
     def equilibriate_vapour(self, temperature: float) -> IdealGasMixture:
-        raise NotImplementedError(
-            "Absent license to MAGMA, implementation must not be shared."
-        )
+        """
+        Reports the vapour composition as function of temperature.
 
-star = Star(
-    name="Sun",
-    mass=1.0 * unit.M_sun,
-    radius=1.0 * unit.R_sun,
-    t_eff=5770.0 * unit.K,
-    distance=10.0 * unit.pc,
-    metallicity=0.0 * unit.dex,
-)
-star.get_spectrum_from_file(
-    outdir="output/stellar_spectra/",
-    source_file=f"input/sun_gueymard_2003_modified.txt",
-    opac_file_for_lambdagrid="input/SiO_opac_ip_kdistr.h5",
-    skiprows=9,
-    plot_and_tweak=False,
-    w_conversion_factor=1e-7,
-    flux_conversion_factor=1e10,
-)
+        Params
+        ------
+            temperature : float
+                Temperature of the melt.
+        
+        Returns
+        -------
+            vapour : IdealGasMixture
+                Object holding the properties of an ideal gas mixture.
+        """
+        pbar = pd.Series()
+        
+        # evaluate species pressure
+        for species, logp_fit in self._logp_fits.items():
+            pbar[species] = 10**float(logp_fit(temperature))
 
-planet = Planet(
-    name="55 Cnc e",
-    mass=1.0 * unit.M_earth,
-    radius=1.0 * unit.R_earth,
-    bond_albedo=0.0,
-    dilution_factor=2.0 / 3.0,
-    internal_temperature=0 * unit.K,
-)
 
-planetary_system = PlanetarySystem(
-    star=star, planet=planet, orbit=CircularOrbitFromPeriod(period=1 * unit.day)
-)
-planetary_system.set_semimajor_axis_from_pl_temp(t_planet=2500 * unit.K)
+        # store as vapour
+        self.vapour = IdealGasMixture(p_bar=pbar)
 
-BSE = {
-    "SiO2": 45,
-    "MgO": 37.8,
-    "FeO": 8.05,
-    "Al2O3": 4.45,
-    "CaO": 3.55,
-    "Na2O": 0.36,
-    "TiO2": 0.201,
-}
-vapour_engine = ModifiedMAGMA(buffer="IW", dlogfO2=4, melt_wt_comp=BSE)
+        return self.vapour
 
-pipeline = PhaethonPipeline(
-    planetary_system=planetary_system,
-    vapour_engine=vapour_engine,
-    outdir="output/test/",
-    opac_species={"SiO"},
-    scatterers={},
-    opacity_path="input/",
-    nlayer=38,
-)
+if __name__=="__main__":
+    star = Star(
+        name="Sun",
+        mass=1.0 * unit.M_sun,
+        radius=1.0 * unit.R_sun,
+        t_eff=5770.0 * unit.K,
+        distance=10.0 * unit.pc,
+        metallicity=0.0 * unit.dex,
+    )
+    star.get_spectrum_from_file(
+        outdir="output/stellar_spectra/",
+        source_file=f"input/sun_gueymard_2003_modified.txt",
+        opac_file_for_lambdagrid=OPACITY_PATH + "SiO_opac_ip_kdistr.h5",
+        skiprows=9,
+        plot_and_tweak=False,
+        w_conversion_factor=1e-7,
+        flux_conversion_factor=1e10,
+    )
 
-# You might need to adept the architecutre to your system.
-pipeline.run(cuda_kws={'arch':'sm_86'})
+    planet = Planet(
+        name="55 Cnc e",
+        mass=1.0 * unit.M_earth,
+        radius=1.0 * unit.R_earth,
+        bond_albedo=0.0,
+        dilution_factor=2.0 / 3.0,
+        internal_temperature=0 * unit.K,
+    )
+
+    planetary_system = PlanetarySystem(
+        star=star, planet=planet, orbit=CircularOrbitFromPeriod(period=1 * unit.day)
+    )
+    planetary_system.set_semimajor_axis_from_pl_temp(t_planet=2500 * unit.K)
+
+    pipeline = PhaethonPipeline(
+        planetary_system=planetary_system,
+        vapour_engine=VapourEngineExample(),
+        outdir="output/test/",
+        opac_species={"SiO"},
+        scatterers={},
+        opacity_path=OPACITY_PATH,
+        nlayer=38,
+    )
+
+    # You might need to adept the architecutre to your system.
+    pipeline.run(cuda_kws={'arch':'sm_86'})

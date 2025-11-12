e82d1053dece9e3ca71b105d80ea4ec769c1a14c
Fabian <fabian.seidler@erdw.ethz.ch>
2025-11-12 19:09:09 +0100

    rollback to compute capability 8.6

--- example.py
+++ example.py
@@ -134,7 +134,7 @@ if __name__ == "__main__":
     )
 
     # You need to adept the architecutre to your system, see README
-    pipeline.run(nvcc_kws={"arch": "sm_89"}, t_abstol=35)
+    pipeline.run(nvcc_kws={"arch": "sm_86"}, t_abstol=35)
 
     # load results from run
     result = PhaethonResult("output/test/")
